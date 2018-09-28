cd0f706fd23ba523ff024b51d0b816dc54e7ad5f
sethipawandeep <pawandeep2007@gmail.com>
2018-09-28 17:45:35 +0530

    Modified run.sh

--- run.sh
+++ run.sh
@@ -91,11 +91,6 @@ echo
 echo "===== MONO DECODING ====="
 echo
 
-echo
-echo "HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-echo
-echo "$decode_cmd"
-
 utils/mkgraph.sh --mono data/lang exp/mono exp/mono/graph || exit 1
 steps/decode.sh --config conf/decode.config --nj $nj --cmd "$decode_cmd" exp/mono/graph data/test exp/mono/decode
 
