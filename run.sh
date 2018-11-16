542f98d200401bb8eb79a862543e85d48ab24023
sethipawandeep <pawandeep2007@gmail.com>
2018-11-16 10:18:26 +0530

    Modified run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ lm_order=1 # language model order (n-gram quantity) - 1 is enough for digits gra
 . utils/parse_options.sh || exit 1
 [[ $# -ge 1 ]] && { echo "Wrong arguments!"; exit 1; }
 # Removing previously created data (from last run.sh execution)
-rm -rf exp mfcc data/train/spk2utt data/train/cmvn.scp data/train/feats.scp data/train/split1 data/test/spk2utt data/test/cmvn.scp data/test/feats.scp data/test/split1 data/local/lang data/lang data/local/tmp data/local/dict/lexiconp.txt
+rm -rf exp mfcc gfcc data/train/spk2utt data/train/cmvn.scp data/train/feats.scp data/train/split1 data/test/spk2utt data/test/cmvn.scp data/test/feats.scp data/test/split1 data/local/lang data/lang data/local/tmp data/local/dict/lexiconp.txt
 
 echo
 echo "===== PREPARING ACOUSTIC DATA ====="
